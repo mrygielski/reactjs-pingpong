ba498d243784b47fdcd9770557a175dee250528f
Michał Rygielski <kontakt@michalrygielski.pl>
2018-04-10 09:12:41 +0200

    change scores

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,9 +4,11 @@ class Results extends React.Component {
     render() {
         return (
             <div className="scores" id="scores">
+                <div>Player</div>
                 <div id="scorePlayer">{this.props.player}</div>
                 <div>:</div>
                 <div id="scoreCpu">{this.props.cpu}</div>
+                <div>CPU</div>
             </div>
         );
     }
